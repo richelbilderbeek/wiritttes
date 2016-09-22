a4decfc3127cf41011482b2dee4e3b303fb7494a
richelbilderbeek <richel@richelbilderbeek.nl>
2016-09-22 13:31:47 +0200

    Can sbatch run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#SBATCH --time=0:10:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
+#SBATCH --mem=1G
+#SBATCH --job-name=run
+#SBATCH --output=run.log
 
 ##########################
 # Clean up

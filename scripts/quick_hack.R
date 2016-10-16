134e683b144793eaed77c88a40941d1d24964751
richelbilderbeek <richel@richelbilderbeek.nl>
2016-10-16 17:45:55 +0200

    Use right folder

--- scripts/quick_hack.R
+++ scripts/quick_hack.R
@@ -1,3 +1,4 @@
 devtools::install_github("richelbilderbeek/wiritttea")
-rda_files <- list.files("~/GitHubs/wiritttes/script", pattern = "\\.RDa", full.names = TRUE)
+rda_files <- list.files("~/GitHubs/wiritttes/scripts", pattern = "\\.RDa", full.names = TRUE)
+print(rda_files)
 wiritttea::collect_files_nltt_stats(rda_files)

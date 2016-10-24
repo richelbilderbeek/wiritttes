aaf65346bf1ec4fdb846f7697d5975b1c30dd949
richelbilderbeek <richel@richelbilderbeek.nl>
2016-10-24 12:11:21 +0200

    test_beast2 script be more verbose

--- scripts/test_beast2.sh
+++ scripts/test_beast2.sh
@@ -6,5 +6,7 @@
 #SBATCH --mem=1G
 #SBATCH --job-name=test_beast2
 #SBATCH --output=test_beast2.log
+echo "Loading modules"
 module load R/3.3.1-foss-2016a
+echo "Starting script"
 time Rscript test_beast2.R

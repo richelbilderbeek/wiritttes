3860e66d02fa57af8316f36c56e7879a503269c1
richelbilderbeek <richel@richelbilderbeek.nl>
2017-05-03 14:39:13 +0200

    Use newer and correct parameters only

--- R/create_parameter_files_article.R
+++ R/create_parameter_files_article.R
@@ -1,23 +1,23 @@
 #' Creates the parameter files in the article
+#' Now every parameter setting has its own seed
 #' @return The filenames of all parameter files created
 #' @export
 #' @author Richel Bilderbeek
-create_parameter_files_article <- function() {
+create_parameter_files_article_20161228 <- function() {
   filenames <- NULL
   b_index <- 0
+  seed <- 100
   for (b in c(0.3, 0.5)) {
     lambda_index <- 0
     for (lambda in c(0.1, 0.3, 1.0, 1000000)) {
       mu_index <- 0
-      for (mu in c(0.0, 0.1, 0.2, 0.4)) {
+      for (mu in c(0.0, 0.1, 0.2)) {
         if (mu > b) next
         r_index <- 3
-        #for (r in c(0.1, 0.01, 0.001)) {
         for (r in c(0.5)) {
           l_index <- 0
           for (l in c(1000, 10000)) {
-            seed_index <- 1
-            for (seed in seq(1, 3)) {
+            for (replicate in seq(1, 20)) {
               filename <- paste0(
                 "article_",
                 b_index, "_",
@@ -25,7 +25,7 @@ create_parameter_files_article <- function() {
                 mu_index, "_",
                 r_index, "_",
                 l_index, "_",
-                seed_index, ".RDa"
+                seed, ".RDa"
               )
               filenames <- c(filenames, filename)
               save_parameters_to_file(
@@ -43,7 +43,7 @@ create_parameter_files_article <- function() {
                 n_beast_runs = 2,
                 filename = filename
               )
-              seed_index <- seed_index + 1
+              seed <- seed + 1
             } # l
             l_index <- l_index + 1
           } # l

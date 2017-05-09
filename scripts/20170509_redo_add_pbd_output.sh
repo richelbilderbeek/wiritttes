124cfad071edaaf425d1968e08e75a2da098d872
richelbilderbeek <richel@richelbilderbeek.nl>
2017-05-09 14:40:32 +0200

    Run sequentially [skip ci]

--- scripts/20170509_redo_add_pbd_output.sh
+++ scripts/20170509_redo_add_pbd_output.sh
@@ -10,12 +10,14 @@
 # These are the files that did not have an incipient species tree
 # 'add_pbd_output.sh' allocated one hour for simulating this tree
 # Was the time insufficient?
-sbatch add_pbd_output.sh /data/p230198/20170509/article_1_3_0_3_0_944.RDa
-sbatch add_pbd_output.sh /data/p230198/20170509/article_1_3_0_3_0_945.RDa
-sbatch add_pbd_output.sh /data/p230198/20170509/article_1_3_0_3_0_947.RDa
-sbatch add_pbd_output.sh /data/p230198/20170509/article_1_3_0_3_0_951.RDa
-sbatch add_pbd_output.sh /data/p230198/20170509/article_1_3_0_3_0_953.RDa
-sbatch add_pbd_output.sh /data/p230198/20170509/article_1_3_0_3_0_958.RDa
-sbatch add_pbd_output.sh /data/p230198/20170509/article_1_3_0_3_1_968.RDa
-sbatch add_pbd_output.sh /data/p230198/20170509/article_1_3_0_3_1_972.RDa
-sbatch add_pbd_output.sh /data/p230198/20170509/article_1_3_0_3_1_976.RDa
+
+# Removed sbatch to test for race conditions
+add_pbd_output.sh /data/p230198/20170509/article_1_3_0_3_0_944.RDa
+add_pbd_output.sh /data/p230198/20170509/article_1_3_0_3_0_945.RDa
+add_pbd_output.sh /data/p230198/20170509/article_1_3_0_3_0_947.RDa
+add_pbd_output.sh /data/p230198/20170509/article_1_3_0_3_0_951.RDa
+add_pbd_output.sh /data/p230198/20170509/article_1_3_0_3_0_953.RDa
+add_pbd_output.sh /data/p230198/20170509/article_1_3_0_3_0_958.RDa
+add_pbd_output.sh /data/p230198/20170509/article_1_3_0_3_1_968.RDa
+add_pbd_output.sh /data/p230198/20170509/article_1_3_0_3_1_972.RDa
+add_pbd_output.sh /data/p230198/20170509/article_1_3_0_3_1_976.RDa

50b93ac5b271ae23c344bb431098a3a99dd41e83
richelbilderbeek <richel@richelbilderbeek.nl>
2017-05-10 10:48:57 +0200

    Redo 958, 972 and 976 [skip ci]

--- scripts/20170510_redo_add_pbd_output_958.sh
+++ scripts/20170510_redo_add_pbd_output_958.sh
@@ -10,9 +10,9 @@
 # These are the files that did not have an incipient species tree
 # 'add_pbd_output.sh' allocated one hour for simulating this tree
 # Was the time insufficient?
-show_parameters.sh /data/p230198/20170509/article_1_3_0_3_0_958.RDa
-add_pbd_output.sh  /data/p230198/20170509/article_1_3_0_3_0_958.RDa
-show_parameters.sh /data/p230198/20170509/article_1_3_0_3_1_972.RDa
-add_pbd_output.sh  /data/p230198/20170509/article_1_3_0_3_1_972.RDa
-show_parameters.sh /data/p230198/20170509/article_1_3_0_3_1_976.RDa
-add_pbd_output.sh  /data/p230198/20170509/article_1_3_0_3_1_976.RDa
+./show_parameters.sh /data/p230198/20170509/article_1_3_0_3_0_958.RDa
+./add_pbd_output.sh  /data/p230198/20170509/article_1_3_0_3_0_958.RDa
+./show_parameters.sh /data/p230198/20170509/article_1_3_0_3_1_972.RDa
+./add_pbd_output.sh  /data/p230198/20170509/article_1_3_0_3_1_972.RDa
+./show_parameters.sh /data/p230198/20170509/article_1_3_0_3_1_976.RDa
+./add_pbd_output.sh  /data/p230198/20170509/article_1_3_0_3_1_976.RDa

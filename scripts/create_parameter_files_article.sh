387a54ba2ebbd6a1daa5cf8b6aa878b4ac39628a
richelbilderbeek <richel@richelbilderbeek.nl>
2017-05-22 13:37:26 +0200

    Allocate more space

--- scripts/create_parameter_files_article.sh
+++ scripts/create_parameter_files_article.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --ntasks=1
-#SBATCH --mem=1M
+#SBATCH --mem=1G
 #SBATCH --job-name=create_parameter_files_article
 #SBATCH --output=create_parameter_files_article.log
 module load R/3.3.1-foss-2016a

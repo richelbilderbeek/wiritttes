1c5b7be596f31c41d1a141b25aeff21373ce2c8b
richelbilderbeek <richel@richelbilderbeek.nl>
2018-03-21 08:38:17 +0100

    Doc

--- R/run.R
+++ R/run.R
@@ -1,33 +1,9 @@
 #' Run one point of the experiment
-#' @param speciation_initiation_rate speciation rate
-#'   of good and incipient species,
-#'   per lineage probability per time unit
-#' @param speciation_completion_rate speciation completion rate (of
-#'   incipient species),
-#'   per lineage probability per time unit
-#' @param extinction_rate extinction rate of good and incipient species,
-#'   per lineage probability per time unit
-#' @param crown_age crown age, in time units
-#' @param crown_age_sigma the standard deviation of MRCA prior's
-#'   distribution, in time units
-#' @param sampling_method method how the incipient species representing a
-#'   species is picked. Must be 'youngest', 'oldest' or 'random'
-#' @param mutation_rate per-nucleotide probability to mutate per time unit
-#' @param sequence_length length of simulated DNA alignment,
-#'   number of base pairs
-#' @param mcmc_length length of the MCMC chain,
-#'   number of MCMC states
-#' @param minimal_ess the minimal Effective Sample Size the posterior
-#'   estimate may be. Will stop if the resulting ESS is below this
-#'   value
-#' @param tree_sim_rng_seed RNG seed used in the creation of the true
-#'   phylogenetic tree
-#' @param alignment_rng_seed RNG seed used in the creation of the simulated
-#'   alignment (from the true phylogenetic tree)
-#' @param beast2_rnd_seed RNG seed in the creation of the BEAST2 posterior
-#'   (based on the alignment)
+#' @param parameters biological and methodological parameters,
+#'   as created by \code{\link{create_params}}
 #' @param beast_jar_path the path of beast.jar
 #' @param verbose set to TRUE for more (debug) output
+#' @author Richel J.C. Bilderbeek
 #' @export
 run <- function(
   parameters,
